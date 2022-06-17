f1d44a020c3844a6f04a1fb2218d3f3ab88eccb5
Kunal Mandalia <kunal.v.mandalia@gmail.com>
2022-06-18 00:33:25 +0100

    feat: add birthday problem

---
+++ birthday_problem/birthday_problem.js
@@ -0,0 +1,26 @@
+// returns # of people until 2 people share birthday
+function simulate() {
+  const birthdays = []
+
+  for (let i = 1; i < 365; i++) {
+    const birthday = Math.ceil(Math.random() * 365) + 1
+    if (birthdays.includes(birthday)) {
+      return i
+    }
+    birthdays.push(birthday)
+  }
+
+  throw Error('Expected overlapping birthdays')
+}
+
+function main() {
+  const simCount = 1000
+  const results = new Array(simCount)
+    .fill(0)
+    .map(simulate)
+    .sort((a, b) => a - b)
+
+  const mean = results.reduce((a, b) => a + b, 0) / results.length
+  console.log({ results, mean })
+}
+main()
